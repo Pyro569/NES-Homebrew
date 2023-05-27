d2682b5f060aec0b33369a573f64f0ab3e5a434b
Nathan Feiler <79991107+Pyro569@users.noreply.github.com>
2023-05-27 00:03:00 -0400

    Add files via upload

--- Hello World/hello_world.asm
+++ Hello World/hello_world.asm
@@ -20,6 +20,10 @@ LoadPalettes:
     LDA #$00
     STA $2006
 
+    STA $2007
+    LDA #$01
+    STA $2007
+
     LDX #$00
 
 LoadPalettesLoop:
@@ -35,7 +39,7 @@ LoadPalettesLoop:
     LDA #$00
     STA $0201
     STA $0202
-
+ 
     LDA #%10000000
     STA $2000
 
@@ -58,7 +62,7 @@ NMI:
 
 PaletteData:
     .db $0F, $31, $32, $33, $0F, $35, $36, $37, $0F, $39, $3A, $3B, $0F, $3D, $3E, $0F
-    .db $0F, $1C, $15, $14, $0F, $02, $38, $3C, $0F, $1C, $15, $14, $0F, $02, $38, $3C 
+    .db $0F, $1C, $15, $14, $0F, $02, $38, $3C, $0F, $1C, $15, $14, $0F, $02, $38, $3C
 
     .org $FFFA
     .dw NMI
